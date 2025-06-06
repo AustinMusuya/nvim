be6e91c45a74d4362c0ab5106312c5530be53ab2
austinmusuya <amusuya57@gmail.com>
2025-06-06 17:21:46 +0300

    chore: import plugins for telescope and lsp from their respective file paths

--- init.lua
+++ init.lua
@@ -33,6 +33,10 @@ require 'plugins.neotree',
 
 require 'plugins.bufferline',
 
+require 'plugins.telescope',
+
+require 'plugins.lsp',
+
 -- See `:help gitsigns` to understand what the configuration keys do
   { -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
